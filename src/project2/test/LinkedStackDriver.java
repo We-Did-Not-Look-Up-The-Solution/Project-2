9e7c7aa50e4752f14e0dc690fb3b52da76ec2468
CarltheSpiny <60937275+CarltheSpiny@users.noreply.github.com>
2022-10-17 16:05:59 -0700

    Update LinkedStackDriver.java
    Should produce the correct infix conversion to postfix (forgot the opening "(" in front of "c-a"))

--- src/project2/test/LinkedStackDriver.java
+++ src/project2/test/LinkedStackDriver.java
@@ -12,15 +12,14 @@ public class LinkedStackDriver {
 
     public static void main(String[] args) {
         // creating the LinkedStack with the infix expression
-        String infixExpression = "a*b/c-a)+d*e";
+        String infixExpression = "a*b/(c-a)+d*e";
         char[] infixArr = infixExpression.toCharArray();
         LinkedStack<Character> LS = new LinkedStack<Character>();
-        for (char ch : infixArr) {
-            LS.push(ch);
-        }
+        for (int index = infixArr.length - 1; index > -1; index--)
+        	LS.push(infixArr[index]);
         String postFixExpression = Calculator.convertToPostFix(LS);
 
         System.out.println("Infix Expression: " + infixExpression);
         System.out.println("Postfix Expression: " + postFixExpression);
-    }
+   }
 }
