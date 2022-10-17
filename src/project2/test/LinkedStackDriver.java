bd418e686c14f22a85f7c066f718a674bf61aef9
richardpacheco00 <richard.a.pacheco.iii@gmail.com>
2022-10-16 18:29:00 -0700

    initial commit, LinkedStackDriver complete. ResizableArrayStackDriver has EmptyStackException issue

--- src/project2/test/LinkedStackDriver.java
+++ src/project2/test/LinkedStackDriver.java
@@ -1,10 +1,26 @@
 package project2.test;
 
+import project2.stacks.*;
+
 /**
  * Test the expression of Task 1 with the algorithim implemented in Task 2
+ * 
  * @author wwwyv
  *
  */
 public class LinkedStackDriver {
 
+    public static void main(String[] args) {
+        // creating the LinkedStack with the infix expression
+        String infixExpression = "a*b/c-a)+d*e";
+        char[] infixArr = infixExpression.toCharArray();
+        LinkedStack<Character> LS = new LinkedStack<Character>();
+        for (char ch : infixArr) {
+            LS.push(ch);
+        }
+        String postFixExpression = Calculator.convertToPostFix(LS);
+
+        System.out.println("Infix Expression: " + infixExpression);
+        System.out.println("Postfix Expression: " + postFixExpression);
+    }
 }
--- src/project2/test/ResizableArrayStackDriver.java
+++ src/project2/test/ResizableArrayStackDriver.java
@@ -1,10 +1,26 @@
 package project2.test;
 
+import project2.stacks.*;
+
 /**
- * Test the expression of Task 1, with the variables of task 4, using the algorithim in Task 5
+ * Test the expression of Task 1, with the variables of task 4, using the
+ * algorithim in Task 5
+ * 
  * @author wwwyv
  *
  */
 public class ResizableArrayStackDriver {
+    public static void main(String[] args) {
+        // creating the ResizableArrayStack with the postfix expression
+        String postFixExpression = "ed*acb/a*-";
+        char[] postFixArr = postFixExpression.toCharArray();
+        ResizableArrayStack<Character> RAS = new ResizableArrayStack<Character>();
+        for (char ch : postFixArr) {
+            RAS.push(ch);
+        }
+
+        System.out.println("Postfix Expression: " + postFixExpression);
+        System.out.println("Postfix Evaluation: " + Calculator.evaluatePostfix(RAS));
+    }
 
 }
